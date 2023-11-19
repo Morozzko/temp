c8cf7decfd2ce80a6dadfd4dc2db69424fc18e94
kovalev <kkovalev@lanit.ru>
2023-11-19 19:54:04 +0300

    add gh pages

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -15,10 +15,9 @@ export const AppRouter: FC = () => (
       <Route index element={<Main />} />
     </Route>
 
-    {/* <Route path={RoutesEnum.admin} element={<Layouts.Secondary />}>*/}
-    {/*  <Route index element={<div>1</div>} />*/}
-    {/*  <Route path={Constants.routes.banned} element={<div>2</div>} />*/}
-    {/* </Route>*/}
+    <Route path="/temp" element={<Layouts.Main />}>
+      <Route index element={<Main />} />
+    </Route>
 
     <Route path="*" element={<NotFound />} />
   </Routes>
